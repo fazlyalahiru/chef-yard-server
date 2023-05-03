379712c05e7ae86aba14ffd2de38046923349a66
fazlyalahiru <fazlyalahi.ru@gmail.com>
2023-05-03 09:19:18 +0600

    data marged

--- index.js
+++ index.js
@@ -2,18 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-const chef = require('./data/chef.json')
-const chefDetails = require('./data/details.json')
+const chefDetails = require('./data/chefDetails.json')
 
 app.get("/", (req, res)=>{
     res.send('chef server is running')
 });
 
 app.use(cors());
-
-app.get('/chef', (req, res)=>{
-    res.send(chef)
-})
 
 app.get('/details', (req, res)=>{
     res.send(chefDetails)
@@ -22,7 +17,7 @@ app.get('/details', (req, res)=>{
 app.get('/details/:id', (req, res)=>{
     const id = req.params.id;
     console.log(id);
-    const selectedChef= chefDetails.find(c=> c._id===id);
+    const selectedChef= chefDetails.find(c=> c.id===id);
     res.send(selectedChef)
 })
 
